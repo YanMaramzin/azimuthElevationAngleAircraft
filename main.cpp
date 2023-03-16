0382626eec9af410f27618cc09e5db0c1308a3ba
Yan Maramzin <yan.maramzin@mail.ru>
2023-03-17 00:28:53 +0300

    Исправление задания

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 /*  Задача №2
     Пеленгатор размещен на летательном аппарате. Известны углы курса, крена и тангажа.
@@ -6,17 +8,13 @@
     направление относительно земли. (Выполнить повороты, относительно осей)
  */
 
-/*  Я нашел, что азимутом относительно самолета называется угол между продольной осью самолета и вертикальной
- *  плоскостью, проходящей через цель.(Подходит ли это определение или здесь имеется ввиду истинный курс?)
- *
- *  Получается, что курсовой угол это наш азимут относительно самолета.
- *  Необходимо для определения азимута относительно Земли, т.е. повернуть оси на угол рысканья и учесть угол крена.
- *  Также по заданию нам известен угол места и нам необходимо найти угол места относительно Земли.
- *  Угол места это угол между направлением на цель и его проекцией на горизонатльную плоскость. Нам дан угол места относительно
- *  горизонтальной плоскости самолета.
- *
- *  Необходимо выровнять самолет, чтобы его оси его связанной системы координат совпадали с местной системой координат.
- */
+
+double conversionFromDegreesToRadians(double degrees);
+std::vector<double> getUnitVector(double azimut, double elevationAngle);
+std::vector<double> rotationAroundAxes(double xAngle, double yAngle, double zAngle, std::vector<double>& coord);
+std::vector<double> multiplicationVectorByMatrix(std::vector<double> vec, std::vector<std::vector<double>> matrix);
+double conversionFromRadiansToDegrees(double radians);
+
 
 
 int main()
@@ -24,31 +22,94 @@ int main()
     double pitchAngle;                  //угол тангажа
     double bankAngle;                   //угол крена
     double headingAngle;                //угол курса
-//    double azimuthRelativeAFSP;         //азимут относительно АФС-П
+    double azimuthRelativeAFSP;         //азимут относительно АФС-П
     double elevationAngleRelativeAFSP;  //угол места относительно АФС-П
 
+
+    std::cout<<"Введите значение азимута относительно АФС-П от 0 до 360 градусов: ";
+    std::cin>>azimuthRelativeAFSP;
+    azimuthRelativeAFSP=conversionFromDegreesToRadians(azimuthRelativeAFSP);
+
+    std::cout<<"Введите угол места относительно АФС-П от -90 до 90 градусов: ";
+    std::cin>>elevationAngleRelativeAFSP;
+    elevationAngleRelativeAFSP=conversionFromDegreesToRadians(elevationAngleRelativeAFSP);
 
     //Ввод необходимых данных для рассчета
     std::cout<<"Введите значение угла тангажа: ";
     std::cin>>pitchAngle;
+    pitchAngle=conversionFromDegreesToRadians(-pitchAngle);
 
     std::cout<<"Введите значение угла крена: ";
     std::cin>>bankAngle;
+    bankAngle=conversionFromDegreesToRadians(bankAngle);
 
     std::cout<<"Введите значение угла курса: ";
     std::cin>>headingAngle;
+    headingAngle=conversionFromDegreesToRadians(-headingAngle);
 
-//    std::cout<<"Введите азимут оносительно АФС-П: ";
-//    std::cin>>azimuthRelativeAFSP;
+    std::vector<double> unitVector=getUnitVector(azimuthRelativeAFSP,elevationAngleRelativeAFSP);
 
-    std::cout<<"Введите угол места относительно АФС-П: ";
-    std::cin>>elevationAngleRelativeAFSP;
 
-    double azimuthRelativeEarth=headingAngle-bankAngle;
-    double elevationAngleRelativeEarth=elevationAngleRelativeAFSP-(pitchAngle+bankAngle);
+    unitVector=rotationAroundAxes(pitchAngle,bankAngle,headingAngle,unitVector);
 
-    std::cout<<"Азимут относительно Земли: "<<azimuthRelativeEarth<<"\n";
-    std::cout<<"Угол места относительно Земли: "<<elevationAngleRelativeEarth<<"\n";
+    double azimuthOverGround=-atan(unitVector[0]/unitVector[1]);
+    double elevationAngleRelativeGround=M_PI-atan(sqrt(unitVector[0]*unitVector[0]+unitVector[1]*unitVector[1])/unitVector[3]);
+
+    std::cout<<"Азимут: "<<conversionFromRadiansToDegrees(azimuthOverGround)<<" в градусах\n";
+    std::cout<<"Угол места: "<<conversionFromRadiansToDegrees(elevationAngleRelativeGround)<<" в градусах\n";
 
     return 0;
+}
+
+double conversionFromDegreesToRadians(double degrees)
+{
+    return degrees*M_PI/180;
+}
+
+double conversionFromRadiansToDegrees(double radians)
+{
+    return radians*180/M_PI;
+}
+
+std::vector<double> getUnitVector(double azimut, double elevationAngle)
+{
+    std::vector<double> unitVector={cos(elevationAngle)*sin(azimut),
+                                    sin(elevationAngle),
+                                    cos(elevationAngle)*cos(azimut)
+                                   };
+    return unitVector;
+}
+
+std::vector<double> multiplicationVectorByMatrix(std::vector<double> vec, std::vector<std::vector<double>> matrix)
+{
+
+    std::vector<double> multVecMatr(vec.size());
+    double sum;
+    for(size_t i=0;i<vec.size();i++)
+    {
+        sum=0;
+        for(size_t j=0; j<matrix[0].size();j++)
+           sum+=vec[j]*matrix[j][i];
+        multVecMatr[i]=sum;
+    }
+    return multVecMatr;
+}
+
+std::vector<double> rotationAroundAxes(double xAngle, double yAngle, double zAngle, std::vector<double>& coord)
+{
+   std::vector<std::vector<double>> xRotationMatrix= {  { 1, 0, 0},
+                                                        { 0, cos(xAngle), sin(xAngle)},
+                                                        { 0, -sin(xAngle), cos(xAngle)}};
+
+   std::vector<std::vector<double>> yRotationMatrix={  { cos(yAngle), 0, sin(yAngle)},
+                                                       { 0, 1, 0},
+                                                       { -sin(yAngle), 0, 0}};
+   std::vector<std::vector<double>> zRotationMatrix={  { cos(zAngle), sin(zAngle), 0},
+                                                       { -sin(zAngle), cos(zAngle), 0},
+                                                       { 0, 0, 1}};
+   std::vector<double> newCoord;
+   newCoord=multiplicationVectorByMatrix(coord,xRotationMatrix);
+   newCoord=multiplicationVectorByMatrix(coord,yRotationMatrix);
+   newCoord=multiplicationVectorByMatrix(coord,zRotationMatrix);
+   return newCoord;
 }
